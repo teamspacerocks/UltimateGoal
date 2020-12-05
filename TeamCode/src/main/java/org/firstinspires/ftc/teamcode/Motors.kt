7e0d69e0ac8b397721029b624d6772f2a6bb422e
DolphinGui <umeda.shin@gmail.com>
2020-12-05 15:53:40 -0800

    changed from Motors:DcMotor map to DcMotors array.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors.kt
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode
 
-enum class Motors {
-    LEFTFRONT, RIGHTFRONT, LEFTBACK, RIGHTBACK, LEFTLAUNCH, RIGHTLAUNCH
+enum class Motors(val i: Int, val s: String) {
+    LF(0, "left_front"),
+    RF(1, "right_front"),
+    LB(2, "left_back"),
+    RB(3, "right_back"),
+    L_LAUNCH(0, "launch1"),
+    R_LAUNCH(1, "launch2");
+
 }
