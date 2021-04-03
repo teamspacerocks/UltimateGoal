e28c0a15b10d666ae92ad43e17cbe5494c32b1af
DolphinGui <umeda.shin@gmail.com>
2021-04-03 14:13:05 -0700

    added Controller.kt as a wrapper. need to add more stuff.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/Controller.kt
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.wrappers
+
+import com.qualcomm.robotcore.hardware.Gamepad
+
+class Controller(var gamepad: Gamepad) {
+    enum class States{
+        PRESS_DOWN, HOLD_DOWN, RELEASE, NEUTRAL
+    }
+    var pad = gamepad
+    private var upPressed = MutibleBool(false)
+    private var downPressed = MutibleBool(false)
+    private var leftPressed = MutibleBool(false)
+    private var rightPressed = MutibleBool(false)
+
+    private class MutibleBool(boolean: Boolean){
+        var d = boolean
+    }
+
+    companion object{//should try to implement callbacks
+        private fun judge(pressed: Boolean, was: MutibleBool): States {
+            val result: States
+            if (pressed) {
+                result = if (was.d)
+                    States.HOLD_DOWN
+                else
+                    States.PRESS_DOWN
+                was.d = true
+
+            } else {
+                result = if (was.d)
+                    States.RELEASE
+                else
+                    States.NEUTRAL
+            }
+            return result
+        }
+    }
+    fun getUp(): States {
+        return judge(gamepad.dpad_up, upPressed)
+    }
+    fun getDown(): States{
+        return judge(gamepad.dpad_down, downPressed)
+    }
+    fun getLeft(): States{
+        return judge(gamepad.dpad_left, leftPressed)
+    }
+    fun getRight(): States {
+        return judge(gamepad.dpad_right, rightPressed)
+    }
+}
